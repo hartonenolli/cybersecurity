b0307c563337c375c3c2441a6c5529b97b1d70ce
hartonenolli <hartonenolli@gmail.com>
2023-11-01 20:55:15 +0200

    Users can post and view messages

--- database_methods.py
+++ database_methods.py
@@ -9,3 +9,28 @@ def get_person(username):
         return person
     except TypeError:
         return None
+
+def get_person_password(username, password):
+    sql = "SELECT password FROM person WHERE username=:username"
+    result = db.session.execute(text(sql), {'username':username})
+    person_password = result.fetchone()[0]
+    if person_password == password:
+        return True
+    return False
+
+def get_person_id(username):
+    sql = "SELECT id FROM person WHERE username=:username"
+    result = db.session.execute(text(sql), {'username':username})
+    person_id = result.fetchone()[0]
+    return person_id
+
+def add_message(message, user_id):
+    sql = "INSERT INTO info_message (user_id, time, memo) VALUES (:user_id, NOW(), :message) "
+    db.session.execute(text(sql), {'user_id':user_id, 'message':message})
+    db.session.commit()
+
+def get_messages():
+    sql = "SELECT * FROM info_message"
+    result = db.session.execute(text(sql))
+    messages = result.fetchall()
+    return messages
--- routes.py
+++ routes.py
@@ -7,18 +7,40 @@ def index():
     if request.method == "GET":
         return render_template('index.html')
 
-@app.route('/front_page', methods=["POST"])
+@app.route('/front_page', methods=["POST", "GET"])
 def front_page():
     if request.method == "POST":
         username = request.form["username"]
+        password = request.form["password"]
         user_exists = database_methods.get_person(username)
-        if user_exists == username:
+        user_password = database_methods.get_person_password(username, password)
+        if user_exists == username and user_password == True:
             session["username"] = username
-            return render_template('front_page.html')
+            messages = database_methods.get_messages()
+            return render_template('front_page.html', messages=messages)
         return redirect('/')
+    if request.method == "GET":
+        messages = database_methods.get_messages()
+        return render_template('front_page.html', messages=messages)
 
 
 @app.route('/logout', methods=["POST"])
 def logout():
     if request.method == "POST":
         return redirect('/')
+
+@app.route('/message', methods=["POST"])
+def message():
+    if request.method == "POST":
+        message = request.form["message"]
+        return render_template('message.html', message=message)
+
+@app.route('/add_message', methods=["POST"])
+def add_message():
+    if request.method == "POST":
+        message = request.form["message"]
+        username = session["username"]
+        user_id = database_methods.get_person_id(username)
+        database_methods.add_message(message, user_id)
+        messages = database_methods.get_messages()
+        return render_template('front_page.html', messages=messages)
